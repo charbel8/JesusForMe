ead0f2aa36b6a2667cfd8ac8df00e1797b273870
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-04 11:17:03 +0300

    Create MariamFoukty.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/MariamFoukty.java
@@ -0,0 +1,40 @@
+package com.charbel.jesusforme.data.tratil;
+
+import com.charbel.jesusforme.data.template;
+
+public class MariamFoukty extends template {
+
+    public MariamFoukty() {
+        super();
+
+        super.title = "يا مريم البكر فقتى";
+        super.data = "<p  dir=\"RTL\">\n"
+                + "يا مريم البكر فقتى<br />\n"
+                + "الشمس والقمرا<br />\n"
+                + "\n"
+                + "وكل نجم بأفلاك<br />\n"
+                + "السماء سرى<br />\n"
+                + "<br />\n"
+                + "<br />\n"
+                + "\n"
+                + "يا نجمة الصبح شعى<br />\n"
+                + "فى معابدنا<br />\n"
+                + "ونورى عقلنا<br />\n"
+                + "والسمع والبصر<br />\n"
+                + "<br />\n"
+                + "<br />\n"
+                + "\n"
+                + "يا أم يسوع يا أمى<br />\n"
+                + "ويا أملى<br />\n"
+                + "لا تهملينى متى<br />\n"
+                + "منى الخطا صدرا<br />\n"
+                + "<br />\n"
+                + "<br />\n"
+                + "\n"
+                + "أنت ملاذى وعونى<br />\n"
+                + "كلما ضعفت<br />\n"
+                + "نفسى وجبرا لقلبى<br />\n"
+                + "كلما انكسرا</p>";
+
+    }
+}

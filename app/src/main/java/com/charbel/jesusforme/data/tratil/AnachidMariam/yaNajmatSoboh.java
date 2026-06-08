549a6faec7510ef86ed3bef5c6e8dc995ac54033
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-08 14:01:00 +0300

    Create yaNajmatSoboh.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/yaNajmatSoboh.java
@@ -0,0 +1,57 @@
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+public class yaNajmatSoboh extends template {
+
+    public yaNajmatSoboh() {
+        super();
+
+        super.title = "يا نجمة الصبح البهيّه  ";
+        super.data = "<p align=\"center\" dir=\"RTL\" >\n"
+                + "يا نجمةَ الصبـح البهيْهْ&nbsp; -&nbsp; وسيـدة كـلِّ&nbsp;\n"
+                + "البريـْه\n"
+                + "<br>\n"
+                + "\n"
+                + "إحمينا مِـِن شرِّ&nbsp;\n"
+                + "الخطيْهْ &nbsp; -&nbsp; يـا امَ يسوعَ الغفـورْ <br>\n"
+                + "\n"
+                + "ررَبي اصطفاكِ&nbsp; -&nbsp; دونَ اشتراكِ (٢\n"
+                + "<br>\n"
+                + "\n"
+                + "\n"
+                + "ذكرُ مريم َينفي الشجون (٢)\n"
+                + "</p>\n"
+                + "\n"
+                + "\n"
+                + "\n"
+                + "<p align=\"center\" dir=\"RTL\" >\n"
+                + "لـمّا رأى باري الدهورِ&nbsp;&nbsp; -&nbsp; جنس الأنـام في الدثور\n"
+                + "<br>\n"
+                + "\n"
+                + "أجرى بكِ روحَ السرورِ&nbsp;&nbsp; &nbsp;-&nbsp;\n"
+                + "وأقامَك الأمَ الطهــور\n"
+                + "<br>\n"
+                + "\n"
+                + "أنتِ شريفه&nbsp;&nbsp; \n"
+                + "-&nbsp; بكرٌ عفيفْهْ (٢)\n"
+                + "<br>\n"
+                + "\n"
+                + "\n"
+                + "ذآكرُ مريم َينفي الشجون (٢)\n"
+                + "\n"
+                + "</p>\n"
+                + "\n"
+                + "<p align=\"center\" dir=\"RTL\" >\n"
+                + "\n"
+                + "أولادَ مريم عظّموهـا&nbsp; -&nbsp; وبالزهـورِ كللــوها\n"
+                + "<br>\n"
+                + "\n"
+                + "هي أمكم فاستشفعوها&nbsp; -&nbsp; وقدّمـوا لها&nbsp;\n"
+                + "الزهـورْ <br>\n"
+                + "\n"
+                + "نشكو النوائبْ&nbsp; -&nbsp; تحت التجاربْ (٢) <br>\n"
+                + "\n"
+                + "ذكرُ مريم َينفي الشجون (٢)</p>";
+    }
+}

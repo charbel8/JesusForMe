d1a153d62206dbb779b1736b482baee4a1f72ed7
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-09 11:41:39 +0300

    Create houbouki.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/houbouki.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+/**
+ *
+ * @author ctannous
+ */
+public class houbouki extends template {
+
+    public houbouki() {
+        super();
+
+        super.title = "حبّك يا مريم ";
+        super.data = "<p  dir=\"RTL\">\n"
+                + "حبك \n"
+                + "يــا مريــــم - غايــُةُ المنـــــــــى\n"
+                + "<br>\n"
+                + "يـا \n"
+                + "أمَّ&nbsp; المعظّــــم\n"
+                + "&nbsp;- آونـــي أمّنـــــــــــا</p>\n"
+                + "<p  dir=\"RTL\">\n"
+                + "ابنـك \n"
+                + "أوصـــــاك - بنــا فــي الصليــب\n"
+                + "<br>\n"
+                + "\n"
+                + "أعطانـــا إّيّـــاك &nbsp;- فـي شخص الحبيب</p>\n"
+                + "<p  dir=\"RTL\">\n"
+                + "\n"
+                + "كـالأم الحنونــــة - بـــك نستعيــــــــن\n"
+                + "<br>\n"
+                + "\n"
+                + "أظهـري المعونــة - منــــك للبنيــــــــن</p>\n"
+                + "\n"
+                + "<p  dir=\"RTL\">\n"
+                + "\n"
+                + "نرجــوك الحمايـة - مــن كـلّ الآفـــــات&nbsp;\n"
+                + "\n"
+                + "<br>\n"
+                + "\n"
+                + "ضاعفـي العنايــة - في وقــت الممــات</p>\n"
+                + "\n"
+                + "<p  dir=\"RTL\">\n"
+                + "لا \n"
+                + "يكـْنْ يــا مريم&nbsp; - حـــظُّ&nbsp;\n"
+                + "العابديـــــــن&nbsp; \n"
+                + "\n"
+                + "<br>\n"
+                + "يومًـا فــي جهنّم&nbsp; - مثــــلَ \n"
+                + "الكافريـــتن&nbsp; \n"
+                + "</p>";
+    }
+}

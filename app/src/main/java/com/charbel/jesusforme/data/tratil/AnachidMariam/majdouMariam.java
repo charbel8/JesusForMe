6c1c6abada1fbf4eef7c1ab044e38de70cb68765
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-09 11:41:18 +0300

    Create majdouMariam.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/majdouMariam.java
@@ -0,0 +1,40 @@
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+public class majdouMariam extends template {
+
+    public majdouMariam() {
+        super();
+
+        super.title = "مجد مريم يتعظّم ";
+        super.data = "<p  dir=\"RTL\">\n"
+                + "مجـد \n"
+                + "مريــم يتعظّم - فـي المشارق والغروب\n"
+                + "<br>\n"
+                + "كرّموهـا عظّموهـا &nbsp;- ملّكوها فـي القلـوب</p>\n"
+                + "<p  dir=\"RTL\">\n"
+                + "\n"
+                + "\n"
+                + "&nbsp;قد رآهـا واصطفاها - ربُّ&nbsp; كـلِّ&nbsp; العالميــن<br>\n"
+                + "\n"
+                + "ووقاها مُـْذْ براهـا - \n"
+                + "كـلَّ&nbsp; محـذورٍ يَشين</p>\n"
+                + "<p  dir=\"RTL\">\n"
+                + "هي رجاآم في شقاآم - فاسمعـوا يـا خاطئيـن<br>\n"
+                + "لا \n"
+                + "تخافـوا أن ُتُوافوا - \n"
+                + "لحماهــا طالبيــن</p>\n"
+                + "<p  dir=\"RTL\">\n"
+                + "\n"
+                + "أذآرينا وانظرينــا - \n"
+                + "نظـرة الأم الحنـون&nbsp; \n"
+                + "\n"
+                + "<br>\n"
+                + "\n"
+                + "أسعدينا ساعدينــا&nbsp; \n"
+                + "&nbsp;-\n"
+                + "عندما نلقى المنــون \n"
+                + "</p>";
+    }
+}

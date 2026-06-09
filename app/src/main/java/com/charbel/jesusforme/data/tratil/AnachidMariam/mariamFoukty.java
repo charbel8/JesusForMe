ee7310ee389a2c84e06c9e2180e49e1ef0395863
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-09 11:40:28 +0300

    Create mariamFoukty.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/mariamFoukty.java
@@ -0,0 +1,34 @@
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+public class mariamFoukty extends template {
+
+    public mariamFoukty() {
+        super();
+
+        super.title = "يا مريم البكر فقت";
+        super.data = "<p  dir=\"RTL\">\n"
+                + "يـا مريُمُ البكُرُ فقـتِ الشمس والقمرا \n"
+                + "<br>\n"
+                + "وكـلّ نجـم بأفـلاك السماءِ ســــــرى</p>\n"
+                + "\n"
+                + "\n"
+                + "<p  dir=\"RTL\">\n"
+                + "يا أمَّ&nbsp;\n"
+                + "يسـوع يـا أمـي ويــا أملــــــــي\n"
+                + "<br>\n"
+                + "لا تهمليني متى \n"
+                + "عنّــي الخطــا صدرَ&nbsp; \n"
+                + "</p>\n"
+                + "\n"
+                + "\n"
+                + "<p  dir=\"RTL\">\n"
+                + "يـا نجمة الصبحِ شعّـي فـي معابدنــا\n"
+                + "<br>\n"
+                + "\n"
+                + "&nbsp;ونــوّري عقلنـا والسمع \n"
+                + "والبصـــــــــرا&nbsp; \n"
+                + "</p>";
+    }
+}

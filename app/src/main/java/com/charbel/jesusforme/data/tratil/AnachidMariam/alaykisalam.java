2bac118b52bf63131256dcc5d7646ead1bc6d5a2
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-09 08:32:37 +0300

    Create alaykisalam.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/alaykisalam.java
@@ -0,0 +1,60 @@
+
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+
+public class alaykisalam extends template {
+
+    public alaykisalam() {
+        super();
+
+        super.title = "عليك السلام بلا ملل";
+        super.data = "<p align=\"center\"  dir=\"RTL\" >\n"
+                + "عليكِ السلاُمُ بلا مَلل&nbsp; -&nbsp; يا نجمـةَ \n"
+                + "البحر والأمل\n"
+                + "<br>\n"
+                + "يا والــــــــدة ربِّ&nbsp;\n"
+                + "الأزل&nbsp; -&nbsp; وهـي بتــــولٌ لم تزَل\n"
+                + "</p>\n"
+                + "\n"
+                + "\n"
+                + "\n"
+                + "<p align=\"center\"  dir=\"RTL\" >\n"
+                + "بحقِّ&nbsp;\n"
+                + "السلام الذي تلاه&nbsp; -&nbsp; &nbsp;جبريـلُ من قِبَـل الإله\n"
+                + "<br>\n"
+                + "أعيدي سلامًا \n"
+                + "فقدنـــاه&nbsp; &nbsp;-\n"
+                + "&nbsp;بما قـد فعلْنا من الزلل\n"
+                + "</p>\n"
+                + "\n"
+                + "\n"
+                + "<p align=\"center\"  dir=\"RTL\" >\n"
+                + "أنتِ الغنيةُ فـي \n"
+                + "النّعم &nbsp;- &nbsp;ونحنُ \n"
+                + "بنـوكِ في عـدَم\n"
+                + "<br>\n"
+                + "ففرِقّي لنـا عنـد \n"
+                + "النـدم&nbsp; -\n"
+                + "&nbsp;فمَـن جاءَك نالَ الأمـل&nbsp;\n"
+                + "</p>\n"
+                + "\n"
+                + "\n"
+                + "<p align=\"center\"  dir=\"RTL\" >\n"
+                + "صرتِ مدى الدهر أمَّنا&nbsp;&nbsp;-&nbsp; &nbsp;وفضلُ حنانِـك عمَّنـا <br>\n"
+                + "لذلك نُلقي هَمنــــــا&nbsp; \n"
+                + "- &nbsp;عليـك فنُصبح في جذل\n"
+                + "</p>\n"
+                + "\n"
+                + "\n"
+                + "<p align=\"center\"  dir=\"RTL\" >\n"
+                + "فلا تقطعي عنّا \n"
+                + "الهبات&nbsp; &nbsp;-&nbsp; &nbsp;حالاً وفي وقتِ الممـاتيا \n"
+                + "<br>\n"
+                + "أمَّ&nbsp; مُعطينا الحيـــــاة&nbsp; \n"
+                + "&nbsp;-&nbsp; &nbsp;باري البرايــــــا من الأزل\n"
+                + "</p>\n"
+                + "";
+    }
+}

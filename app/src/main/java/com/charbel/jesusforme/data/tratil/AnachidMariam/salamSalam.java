2278477b422634b7e9bbed56a0f8c8996fddb6c4
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-09 08:43:45 +0300

    Create salamSalam.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/salamSalam.java
@@ -0,0 +1,58 @@
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+public class salamSalam extends template {
+
+    public salamSalam() {
+        super();
+
+        super.title = "سلام، سلام";
+        super.data = "<p align=\"center\"  dir=\"RTL\" >\n"
+                + "\n"
+                + "<b>\n"
+                + "سلام، سلام - \n"
+                + "لـكِ يــا مريــم</b>\n"
+                + "</p>\n"
+                + "\n"
+                + "\n"
+                + "<p align=\"center\"  dir=\"RTL\">\n"
+                + "\n"
+                + "جبريـلُ \n"
+                + "وافـــاكِ &nbsp;-\n"
+                + "&nbsp;يُهديــك الســلام \n"
+                + "\n"
+                + "<br>\n"
+                + "\n"
+                + "هـاتفًــا \n"
+                + "طوبــاكِ &nbsp;- \n"
+                + "&nbsp;&nbsp;يــا فخـرَ الأنــــام</p>\n"
+                + "\n"
+                + "<p align=\"center\"  dir=\"RTL\">\n"
+                + "مريـمُ \n"
+                + "طوبـــــاكِ &nbsp;-\n"
+                + "&nbsp;يــا أمَّ&nbsp;\n"
+                + "الحيـــــــاة<br>\n"
+                + "\n"
+                + "ثمـرةُ \n"
+                + "أحشــــاكِ&nbsp; -\n"
+                + "&nbsp;ربُّ&nbsp;\n"
+                + "الكائنــــــــات</p>\n"
+                + "\n"
+                + "\n"
+                + "<p align=\"center\"  dir=\"RTL\" >\n"
+                + "\n"
+                + "الربُّ&nbsp; أعطــــــــاكِ\n"
+                + "&nbsp;- &nbsp;مجـدًا \n"
+                + "لا يـــــــزول&nbsp;<br>\n"
+                + "\n"
+                + "لــم يُعْــطَ سواكِ &nbsp;- &nbsp;&nbsp;&nbsp;يــا أمًــا بتــــــــول</p>\n"
+                + "\n"
+                + "\n"
+                + "<p align=\"center\"  dir=\"RTL\" >\n"
+                + "مـْنْ مثلُـكِ يرْحَمْ &nbsp;- &nbsp;&nbsp;&nbsp;بيــن الأمهــــــــــاتْ\n"
+                + "<br>\n"
+                + "لا تنسينا مَريـــمْ &nbsp;- &nbsp;&nbsp;فــي وقـتِ الممــات\n"
+                + "</p>";
+    }
+}

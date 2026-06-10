04ae473cf406609faae5ccd0c90c3d8bc7302466
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-10 17:01:10 +0300

    Create antichafih.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/antichafih.java
@@ -0,0 +1,33 @@
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+public class antichafih extends template {
+
+    public antichafih() {
+        super();
+
+        super.title = "أنت الشفيع الأكرم";
+        super.data = "<p  dir=\"RTL\" >\n"
+                + "أنتِ الشفيعُ الأكـــرمُ - عنـدَ ابنـكِ يـا مريـــــمُ</p>\n"
+                + "<p  dir=\"RTL\" >\n"
+                + "\n"
+                + "حُزتِ مقامًا في السـما - فوق الخلائـقِ قـد سما\n"
+                + "<br>\n"
+                + "\n"
+                + "وبلغتِ في التقديس ما - عنـهُ الخلائقُ تُحجِـــم</p>\n"
+                + "<p  dir=\"RTL\" >\n"
+                + "\n"
+                + "&nbsp;جبريل وافـى الناصـــرة - ببشارةٍ لكِ باهـــــــرة<br>\n"
+                + "\n"
+                + "قال افرحي يا طاهـــرة - ولـكِ الهنـاءُ الأعظــــمُ</p>\n"
+                + "<p  dir=\"RTL\" >\n"
+                + "\n"
+                + "ربُّ&nbsp; \n"
+                + "الملائـك والبشـــــر - يُهديك مجـدًا مُفتخَـــر\n"
+                + "<br>\n"
+                + "\n"
+                + "إنّ \n"
+                + "المسيـحَ المنتظــــر- مـن جسمـك يتجسَّم</p>";
+    }
+}

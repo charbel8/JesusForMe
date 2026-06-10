143e196f2fff6d92666e51331b66ef7516904807
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-10 09:02:20 +0300

    Create fiZillHimayitiki.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/fiZillHimayitiki.java
@@ -0,0 +1,36 @@
+
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+
+public class fiZillHimayitiki extends template {
+
+    public fiZillHimayitiki() {
+        super();
+
+        super.title = "في ظلّ حمايتك";
+        super.data = "<p  dir=\"RTL\">\n"
+                + "في ظلِّ&nbsp;\n"
+                + "حمايتك - نلتجـُئُ يــا مريم\n"
+                + "<br>\n"
+                + "لا تـردّي طلبتنا \n"
+                + "- عندمــا ندعــوك&nbsp;</p>\n"
+                + "\n"
+                + "\n"
+                + "<p  dir=\"RTL\">\n"
+                + "يـا فخـر \n"
+                + "البرايــا - يـــا خيــــرَ الورى<br>\n"
+                + "يـا بحـر \n"
+                + "العطايـا - للدنيـــــــا جــرى</p>\n"
+                + "\n"
+                + "<p  dir=\"RTL\">\n"
+                + "&nbsp;يـا بـاب السمـاءِ - يــــا أمَّ&nbsp; الفـــــدي<br>\n"
+                + "يـا عيــن الرجاءِ - يــا نــــــورَ الهـدى</p>\n"
+                + "\n"
+                + "<p  dir=\"RTL\">\n"
+                + "إرحمــي عبيــدًا - باتـــوا مخلصيــنْ<br>\n"
+                + "&nbsp; يبغــون المزيــدا - منــكِ كــلّ \n"
+                + "حين</p>";
+    }
+}

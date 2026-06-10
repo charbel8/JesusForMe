c8f16a5b3737e3576bdda78cce201678cf0beabc
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-10 09:03:25 +0300

    Create rayaIftikhar class with title and data
    This class extends the template class and initializes the title and data fields with content related to 'يا راية افتخار'.

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/rayaIftikhar.java
@@ -0,0 +1,38 @@
+
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+
+public class rayaIftikhar extends template {
+
+    public rayaIftikhar() {
+        super();
+
+        super.title = "يا راية افتخار";
+        super.data = "<p  dir=\"RTL\"><b>\n"
+                + "يا \n"
+                + "نورَنا يــا نصرَنا - &nbsp;&nbsp;يا فخرَنا سيّدة \n"
+                + "لبنان</b> (٢)</p>\n"
+                + "\n"
+                + "<p  dir=\"RTL\">\n"
+                + "يا \n"
+                + "راية افتخار فوق لبنان - زَّينتِ بانتصار حُصنَ ملجانا\n"
+                + "<br>\n"
+                + "فجميعُ المؤمنيـن - &nbsp;بإيمــان الضارعينــا\n"
+                + "<br>\n"
+                + "لاذوا مـن العثـار - &nbsp;&nbsp;بالحمـــــى \n"
+                + "الآن</p>\n"
+                + "\n"
+                + "\n"
+                + "<p  dir=\"RTL\">\n"
+                + "في بُرجك الأميـن نرفع الأصـــوات\n"
+                + "<br>\n"
+                + "وحصنك المتين ُنُكثـــرُ الطلبــــــــــات\n"
+                + "<br>\n"
+                + "لتُعيدي الغائبـين وتُعيني النازلينـا\n"
+                + "<br>\n"
+                + "يا منار المُستَعين في دجى الظلمات\n"
+                + "</p>";
+    }
+}

b9ff4aaec34b782a5bae176ee418fee7467ae9b6
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-10 09:01:58 +0300

    Create maljaElBanin.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/maljaElBanin.java
@@ -0,0 +1,37 @@
+
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+
+public class maljaElBanin extends template {
+
+    public maljaElBanin() {
+        super();
+
+        super.title = "عذراء يا ملجا البنين";
+        super.data = "<p dir=\"RTL\" >\n"
+                + "عـذراء يا ملجا البنيــن - جِئنـا لديــك طالبين\n"
+                + "<br>\n"
+                + "كوني \n"
+                + "لنا خيـرَ معيـــن - يـا سيّــدة الانتصـار\n"
+                + "</p>\n"
+                + "\n"
+                + "<p dir=\"RTL\" >\n"
+                + "أعطـي \n"
+                + "قلوبنا السلام - مـن قلبك \n"
+                + "كنـز \n"
+                + "النعم\n"
+                + "<br>\n"
+                + "فإنّه \n"
+                + "مينــا الأنــــــــام - وملجـٌأ لكـلّ بـــــــار\n"
+                + "</p>\n"
+                + "\n"
+                + "<p dir=\"RTL\" >\n"
+                + "أنتِ \n"
+                + "البتـول الطاهــرة - فـي كلّ شيء قادرة\n"
+                + "<br>\n"
+                + "نعماتـك هـي ظاهرة - فـي كلّ دهـر وديــــار\n"
+                + "</p>";
+    }
+}

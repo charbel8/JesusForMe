a96720a11890f312ad59dbe2c0efaa45ab22519f
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-10 09:03:04 +0300

    Create yaOumana.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/yaOumana.java
@@ -0,0 +1,43 @@
+
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+
+public class yaOumana extends template {
+
+    public yaOumana() {
+        super();
+
+        super.title = "يا أمّنا يا مريم العذراء";
+        super.data = "<p  dir=\"RTL\">\n"
+                + "يا أمّنا يا أمّنا يا مريم العذراء&nbsp;\n"
+                + "<br>\n"
+                + "شريكة شريكة أنتِ بالفداء&nbsp; \n"
+                + "<br>\n"
+                + "شفيعة لنا يا أمّنا بالُيُسرِ والصعاب&nbsp;<br>\n"
+                + "يا أمّنا.</p>\n"
+                + "\n"
+                + "\n"
+                + "<p  dir=\"RTL\">\n"
+                + "جبريل ناداك في السرّ ناجاكِ&nbsp;\n"
+                + "<br>\n"
+                + "هيّا إلى الدنيا التي سمعت همسًا بلقياكِ\n"
+                + "<br>\n"
+                + "أعطي حنانك لا تضنّي\n"
+                + "<br>\n"
+                + "لكم طلبناك وتقنا للقياك&nbsp; \n"
+                + "\n"
+                + "\n"
+                + "<br>\n"
+                + "سلامنا بسلامك وحياتنا بحياتك\n"
+                + "<br>\n"
+                + "كم قد غرقنا ببعدنا عنك&nbsp; \n"
+                + "<br>\n"
+                + "وكاد الموج يجرفنا&nbsp; \n"
+                + "<br>\n"
+                + "\n"
+                + "لولا استغاثتنا بك \n"
+                + "</p>";
+    }
+}

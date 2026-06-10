099919452cee5548ff227fb59201780656d0c965
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-10 17:00:50 +0300

    Create kanzBaraket.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/kanzBaraket.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+/**
+ *
+ * @author ctannous
+ */
+public class kanzBaraket extends template {
+
+    public kanzBaraket() {
+        super();
+
+        super.title = "يا أمّ اﷲ كنـز البرآات";
+        super.data = "<p  dir=\"RTL\">\n"
+                + "يا أمّ اﷲ كنـز \n"
+                + "البرآات، \n"
+                + "<br>\n"
+                + "عنك الأنبيا \n"
+                + "أخبروا في البرايا، برموزٍ وآيات.&nbsp; \n"
+                + "\n"
+                + "<br>\n"
+                + "فيكِ تمّت الأسرار \n"
+                + "<br>\n"
+                + "كمُلت النبوءات، وابتدا الميثاق الجديد\n"
+                + "<br>\n"
+                + "يا أمّ الرحمان، يا فخر الأكوان، \n"
+                + "<br>\n"
+                + "يا فيض الرضوان، قد شاء منك الأسمى أن \n"
+                + "يبني له جسما&nbsp; جاء ينقذ الإنسان،\n"
+                + "<br>\n"
+                + "من أظافر السابي، يُعطيه الصفح \n"
+                + "والحنان.&nbsp; \n"
+                + "<br>\n"
+                + "أرزتي الخضراء، تلثم السماء،&nbsp;&nbsp;\n"
+                + "\n"
+                + "<br>\n"
+                + "رمز للعذراء عيناها صوب لبنان، والقلب دومًا سهران.&nbsp;\n"
+                + "\n"
+                + "<br>\n"
+                + "ها إنّ عين الأدهار،\n"
+                + "<br>\n"
+                + "نحو الأرزة الخضراء، إحميها من عين الأدهار.\n"
+                + "</p>";
+    }
+}

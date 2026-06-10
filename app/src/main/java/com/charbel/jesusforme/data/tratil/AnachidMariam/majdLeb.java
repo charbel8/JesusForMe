60955d269720f4ad19f77fc2a74c1175a5069ef9
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-10 09:03:40 +0300

    Create majdLeb.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/majdLeb.java
@@ -0,0 +1,35 @@
+
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+public class majdLeb extends template {
+
+    public majdLeb() {
+        super();
+
+        super.title = "مجــد لبنــان";
+        super.data = "<p  dir=\"RTL\" >\n"
+                + "مجدُ \n"
+                + "لبنان أعطيَ لها\n"
+                + "<br>\n"
+                + "وبهاء \n"
+                + "الكـرمل والشارون&nbsp; <b>هللويا</b>\n"
+                + "<br>\n"
+                + "تفرح \n"
+                + "البرية والقفر يبتهج\n"
+                + "<br>\n"
+                + "\n"
+                + "وتُزهـر الورود لك يا مريم\n"
+                + "<br>\n"
+                + "سترى \n"
+                + "الشعوب إكليلَ مجدك\n"
+                + "<br>\n"
+                + "يا \n"
+                + "تاجًا صانه بكفه الإله\n"
+                + "<br>\n"
+                + "تنشد \n"
+                + "البوادي ترنيـم فرح\n"
+                + "إذ تُبصر بك مجدَ إلهنـا.</p>";
+    }
+}

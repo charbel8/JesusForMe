f6e4785693470497e3da2f7f088b7ac06642a6cc
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-06-10 09:02:47 +0300

    Create sallwaKouloub.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/tratil/AnachidMariam/sallwaKouloub.java
@@ -0,0 +1,36 @@
+
+package com.charbel.jesusforme.data.tratil.AnachidMariam;
+
+import com.charbel.jesusforme.data.template;
+
+public class sallwaKouloub extends template {
+
+    public sallwaKouloub() {
+        super();
+
+        super.title = "sallwaKouloub";
+        super.data = "<p  dir=\"RTL\">\n"
+                + "سلوى \n"
+                + "القلوب قلبُ العذراء - نوُرُ الدروبِ نحو السماء\n"
+                + "<br>\n"
+                + "إن \n"
+                + "خطانا تهفـو إليــــــك - نُلقي شقانـا بين يديك</p>\n"
+                + "<p  dir=\"RTL\">\n"
+                + "يا \n"
+                + "أمُّ&nbsp; جولـي ملءَ الربيع -\n"
+                + "&nbsp;&nbsp;بين الحقـول بين الربوع\n"
+                + "<br>\n"
+                + "صوتُ \n"
+                + "الصلاةِ فوقَ الجبال - خلّيه يعلـو طول الليالي</p>\n"
+                + "<p  dir=\"RTL\">\n"
+                + "&nbsp; \n"
+                + "\n"
+                + "هـلاّ \n"
+                + "دفعـتِ عنّا البـــــلاء - هلاّ بعثت فينـا الرجاءَ\n"
+                + "<br>\n"
+                + "لا شيءَ يُغني عن كلّ لاه - &nbsp;إلّا&nbsp;\n"
+                + "التغنّي \"يـا أم اﷲ\"!\n"
+                + "\n"
+                + "</p>";
+    }
+}

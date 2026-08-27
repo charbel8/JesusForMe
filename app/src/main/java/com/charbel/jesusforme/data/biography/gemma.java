eda8bb9a0b7929b61460594b540873feeff06510
Charbel Tannous <52323275+charbel8@users.noreply.github.com>
2026-08-27 08:19:00 +0300

    Create gemma.java

---
+++ app/src/main/java/com/charbel/jesusforme/data/biography/gemma.java
@@ -0,0 +1,13 @@
+package com.charbel.jesusforme.data.biography;
+
+import com.charbel.jesusforme.data.template;
+
+public class gemma extends template {
+
+    public gemma() {
+        super();
+
+        super.title = "";
+        super.data = "";
+    }
+}
